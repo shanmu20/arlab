b163fcf0ed44e4ad05179eca8ef674616b46cd51
shanmu <b.shanmu20@gmail.com>
2017-10-14 23:42:58 +0530

    jquery added to project

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,14 @@
-var element = document.getElementsByClassName('a-canvas')[0];
-var hammertime = new Hammer(element);
-element.addEventListener('click',function(){
-	console.log('ok');
-});
-hammertime.get('pinch').set({ enable: true });
-hammertime.on('pinch', function(ev) {
-	console.log(ev);
-	var battle_ship = document.getElementById('battleship');
-	battle_ship.prop('scale','0.8 0.8 0.8');
+$(document).read(function(){
+	var element = document.getElementsByClassName('a-canvas')[0];
+	var hammertime = new Hammer(element);
+	element.addEventListener('click',function(){
+		console.log('ok');
+	});
+	hammertime.get('pinch').set({ enable: true });
+	hammertime.on('pinch', function(ev) {
+		console.log(ev);
+		var battle_ship = $('#battleship');
+		battle_ship.attr('scale','0.8 0.8 0.8');
+	});
+
 });
