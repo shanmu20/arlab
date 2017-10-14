5283c9c07d350f71a7b6147af66d972c3a9d48f2
shanmu <b.shanmu20@gmail.com>
2017-10-15 00:18:51 +0530

    hammer.js added

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,15 @@
 $(document).ready(function(){
 	var element = document.getElementsByClassName('a-canvas')[0];
 	var hammertime = new Hammer(element);
-	element.addEventListener('click',function(){
-		console.log('ok');
-	});
 	hammertime.get('pinch').set({ enable: true });
 	hammertime.on('pinch', function(ev) {
 		console.log(ev);
 		if (ev.type === 'pinchstart') {
 			var battle_ship = $('#battleship');
 			var scale_val = battle_ship.attr('scale');
-			var scale_x = scale_val.x * .5;
-			var scale_y = scale_val.y * .5;
-			var scale_z = scale_val.z * .5;
+			var scale_x = scale_val.x * ev.scale;
+			var scale_y = scale_val.y * ev.scale;
+			var scale_z = scale_val.z * ev.scale;
 			var final_val = scale_x + ' ' + scale_y + ' ' + scale_z;
 			battle_ship.attr('scale',final_val);
 		}
