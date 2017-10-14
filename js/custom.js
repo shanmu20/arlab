f754d8b474f932a98591d67cab9cc64a1e881f2d
shanmu <b.shanmu20@gmail.com>
2017-10-15 00:06:04 +0530

    hammer.js experiment

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-$(document).read(function(){
+$(document).ready(function(){
 	var element = document.getElementsByClassName('a-canvas')[0];
 	var hammertime = new Hammer(element);
 	element.addEventListener('click',function(){
@@ -7,8 +7,15 @@ $(document).read(function(){
 	hammertime.get('pinch').set({ enable: true });
 	hammertime.on('pinch', function(ev) {
 		console.log(ev);
-		var battle_ship = $('#battleship');
-		battle_ship.attr('scale','0.8 0.8 0.8');
+		if (ev.type === 'pinchstart') {
+			var battle_ship = $('#battleship');
+			var scale_val = battle_ship.attr('scale');
+			var scale_x = scale_val.x * .5;
+			var scale_y = scale_val.y * .5;
+			var scale_z = scale_val.z * .5;
+			var final_val = scale_x + ' ' + scale_y + ' ' + scale_z;
+			battle_ship.attr('scale',final_val);
+		}
 	});
 
 });
