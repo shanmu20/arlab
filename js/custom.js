7aa736fb0f708717e3cca2d43b9f7a2ccc16fd5e
shanmu <b.shanmu20@gmail.com>
2017-10-14 23:08:18 +0530

    hammer.js testing with div

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,6 @@
-(function(){
-	var element = document.getElementById('battleship');
-	var hammertime = new Hammer(element);
-	hammertime.get('pinch').set({ enable: true });
-	hammertime.on('pinch', function(ev) {
-		console.log(ev);
-	});
-})();
+var element = document.getElementById('battle_div');
+var hammertime = new Hammer(element);
+hammertime.get('pinch').set({ enable: true });
+hammertime.on('pinch', function(ev) {
+	console.log(ev);
+});
