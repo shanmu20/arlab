6a0eb45139a317584aea274a67e87ec7ffaf9242
shanmu <b.shanmu20@gmail.com>
2017-10-14 23:37:07 +0530

    hammerjs events

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,11 @@
-var element = document.getElementById('battle_div');
+var element = document.getElementsByClassName('a-canvas')[0];
 var hammertime = new Hammer(element);
+element.addEventListener('click',function(){
+	console.log('ok');
+});
 hammertime.get('pinch').set({ enable: true });
 hammertime.on('pinch', function(ev) {
 	console.log(ev);
+	var battle_ship = document.getElementById('battleship');
+	battle_ship.prop('scale','0.8 0.8 0.8');
 });
