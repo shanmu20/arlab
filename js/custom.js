5e60332a8e658c357b818026486ac691e651fd44
shanmu <b.shanmu20@gmail.com>
2017-10-14 20:42:12 +0530

    js added in the bottom

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,21 @@
 (function(){
 	var element = document.getElementById('battleship');
-	console.log(element);
-	console.log('ok');
+	var mc = new Hammer.Manager(element);
+
+	// create a pinch and rotate recognizer
+	// these require 2 pointers
+	var pinch = new Hammer.Pinch();
+	var rotate = new Hammer.Rotate();
+
+	// we want to detect both the same time
+	pinch.recognizeWith(rotate);
+
+	// add to the Manager
+	mc.add([pinch, rotate]);
+
+
+	mc.on("pinch rotate", function(ev) {
+		console.log(ev.type);
+	});
+
 })();
