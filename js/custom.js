26f0aa0a5118e34e05b2e95f746b30f230756cb7
shanmu <b.shanmu20@gmail.com>
2017-10-14 21:58:46 +0530

    hammer.js testing

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,8 @@
 (function(){
 	var element = document.getElementById('battleship');
-	var mc = new Hammer.Manager(element);
-
-	// create a pinch and rotate recognizer
-	// these require 2 pointers
-	var pinch = new Hammer.Pinch();
-	var rotate = new Hammer.Rotate();
-
-	// we want to detect both the same time
-	pinch.recognizeWith(rotate);
-
-	// add to the Manager
-	mc.add([pinch, rotate]);
-
-
-	mc.on("pinch rotate", function(ev) {
-		console.log(ev.type);
+	var hammertime = new Hammer(element);
+	hammertime.get('pinch').set({ enable: true });
+	hammertime.on('pinch', function(ev) {
+		console.log(ev);
 	});
-
 })();
