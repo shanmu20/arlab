a96555f427ee249c7166b15a97fa9f679aa6fa58
shanmu <b.shanmu20@gmail.com>
2017-11-04 17:38:49 +0530

    scaling functionality

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,30 @@
 $(document).ready(function(){
-	var element = document.getElementsByClassName('a-canvas')[0];
-	var hammertime = new Hammer(element);
-	hammertime.get('pinch').set({ enable: true });
-	hammertime.on('pinch', function(ev) {
-		//var entity = $('.asset-entity');
-		var entity =  document.querySelectorAll('.asset-entity');
-		var entity_len = entity.length;
-		for (var i=0; i<entity_len; i++){
-			var scale_val =  entity[i].getAttribute('scale');
-			if (ev.type === 'pinch' && ev.additionalEvent == "pinchout") {
-				var scale_x = scale_val.x + scale_val.x/10;
-				var scale_y = scale_val.y + scale_val.y/10;
-				var scale_z = scale_val.z + scale_val.z/10;
-				var final_val = scale_x + ' ' + scale_y + ' ' + scale_z;
-				
-			}else if(ev.type === 'pinch'  && ev.additionalEvent == "pinchin"){
-				if(scale_val.x>=0){
-					var scale_x = scale_val.x - scale_val.x/10;
-					var scale_y = scale_val.y - scale_val.y/10;
-					var scale_z = scale_val.z - scale_val.z/10;
-					var final_val = scale_x + ' ' + scale_y + ' ' + scale_z;
+	setTimeout(function(){
+			var element = document.getElementsByClassName('a-canvas')[0];
+			var hammertime = new Hammer(element);
+			hammertime.get('pinch').set({ enable: true });
+			hammertime.on('pinch', function(ev) {
+				//var entity = $('.asset-entity');
+				var entity =  document.querySelectorAll('.asset-entity');
+				var entity_len = entity.length;
+				for (var i=0; i<entity_len; i++){
+					var scale_val =  entity[i].getAttribute('scale');
+					if (ev.type === 'pinch' && ev.additionalEvent == "pinchout") {
+						var scale_x = scale_val.x + scale_val.x/10;
+						var scale_y = scale_val.y + scale_val.y/10;
+						var scale_z = scale_val.z + scale_val.z/10;
+						var final_val = scale_x + ' ' + scale_y + ' ' + scale_z;
+						
+					}else if(ev.type === 'pinch'  && ev.additionalEvent == "pinchin"){
+						if(scale_val.x>=0){
+							var scale_x = scale_val.x - scale_val.x/10;
+							var scale_y = scale_val.y - scale_val.y/10;
+							var scale_z = scale_val.z - scale_val.z/10;
+							var final_val = scale_x + ' ' + scale_y + ' ' + scale_z;
+						}
+					}
+					entity[i].setAttribute('scale',final_val);
 				}
-			}
-			entity[i].setAttribute('scale',final_val);
-		}
-	});
+			});
+	},1000);
 });
